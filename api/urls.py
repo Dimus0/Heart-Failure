88d64ecb735fb1124a257eb19946d48093119d73
Puhachevskyi Dmytro <106034451+Dimus0@users.noreply.github.com>
2024-12-22 12:26:16 +0200

    /

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, re_path,include
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
-from .views import PatientIndicatorsView,get_recommendations,GetRecommendationsView
+from .views import PatientIndicatorsView,GetRecommendationsView
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('model_predict/', get_recommendations),
-    path('model/',GetRecommendationsView.as_view(), name='model'),
     path('api/heart-risk/predict', GetRecommendationsView.as_view(), name='heart-risk-predict'),
 ]
