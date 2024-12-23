87bad3ed0423bdf8b75e42373592f7621f66b026
Puhachevskyi Dmytro <106034451+Dimus0@users.noreply.github.com>
2024-12-23 16:03:05 +0200

    fix + add + test

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, re_path,include
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 from .views import PatientIndicatorsView,GetRecommendationsView
 from django.views.generic import TemplateView
